1d58c3675a3039cb81b9c7775dea1d551ebaf74f
teklynk <ryanjones153@gmail.com>
2021-12-21 01:10:47 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Twitch Queue
+
+### What is this?
+
+Ever needed to generate a list of users that want to play a game, request a song or video. Or maybe you just want to
+display everyone that used the !fart command. This will create a list containing the users name and avatar. The list has
+a limit that you define. Let's say that you are looking for 7 people from chat to join a game. Chat can type !play until
+the limit is reached.
+
+This project uses [TMI.JS](https://tmijs.com/) to connect to Twitch chat anonymously.
+
+### URL Parameters
+
+**channel** = channel name
+
+**command** = The command to listen for. ie: play, request, next. No need to include the !
+
+**limit** = The number of users to show
+
+**size** = The size of the avatar and username
+
+### [Try it here](https://twitch-queue.pages.dev/)
+
+### Custom CSS
+
+Add this CSS to the OBS browser source and modify as needed.
+
+```
+.queueitem {
+    margin: 0 0 4px 0;
+}
+
+.profileimage img {
+    border-radius: 100%;
+    vertical-align: middle;
+    border: solid 1px black;
+}
+.displayname {
+    font-family: Basic;
+    vertical-align: middle;
+    padding: 0 0 0 4px;
+    font-weight: bold;
+    color: white;
+    text-shadow: 1px 1px #000000;
+}
+```
---
+++ assets/js/overlay.js
@@ -0,0 +1,75 @@
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    let regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    let results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+let channelName = getUrlParameter('channel');
+let queueLimit = getUrlParameter('limit');
+let queueCommand = getUrlParameter('command');
+let size = getUrlParameter('size');
+let profileImage = 'assets/images/default.jpg';
+let queueList = '';
+
+if (!channelName) {
+    alert('Channel Name is not set');
+}
+if (!queueLimit) {
+    alert('Limit is not set');
+}
+if (!queueCommand) {
+    alert('Chat command is not set');
+}
+if (!size) {
+    alert('Size is not set');
+}
+
+// Twitch API: user info: user_id
+function getInfo(channelName, callback) {
+    let urlI = "https://twitchapi.teklynk.com/getuserinfo.php?channel=" + channelName + "";
+    let xhrI = new XMLHttpRequest();
+    xhrI.open("GET", urlI);
+    xhrI.onreadystatechange = function () {
+        if (xhrI.readyState === 4 && xhrI.status === 200) {
+            callback(JSON.parse(xhrI.responseText));
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    xhrI.send();
+}
+
+const client = new tmi.Client({
+    channels: [channelName]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    if (self || !message.startsWith('!')) return;
+
+    const args = message.slice(1).split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === queueCommand) {
+        let countElements = $('.queueitem').length;
+        if (countElements !== parseInt(queueLimit)) {
+            if (tags['display-name'] !== $('.displayname').html()) {
+                getInfo(tags['username'], function (data) {
+                    profileImage = data.data[0]['profile_image_url'];
+
+                    let imageSize = "max-height:" + parseInt(size) * 2 + "px;";
+                    let fontSize = "font-size:" + size + "px;";
+
+                    queueList = '<div class="queueitem"><span class="profileimage"><img style="' + imageSize + '" src="' + profileImage + '" alt=""/></span>' +
+                        '<span class="displayname" style="' + fontSize + '">' + tags['display-name'] + '</span></div>';
+
+                    $(queueList).appendTo('#container');
+                });
+            }
+        }
+    }
+});
